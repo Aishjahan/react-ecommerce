8b9f50fc202fb3d4f7235c77a8d2889dca07aaca
Aishjahan <aishjahankhan7376@gmail.com>
2023-09-18 22:13:52 +0530

    Cart update and remove

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
@@ -15,6 +15,9 @@ import {
 } from "react-router-dom";
 import Cart from "./features/Cart/Cart";
 import Protected from "./features/auth/components/Protected";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchItemsByUserIdAsync } from "./features/Cart/cartSlice";
+import { selectLoggedInUser } from "./features/auth/authSlice";
 
 const router = createBrowserRouter([
   {
@@ -60,6 +63,15 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+
+  useEffect(()=>{
+    if(user){
+      dispatch(fetchItemsByUserIdAsync(user.id));
+    }    
+  },[dispatch,user]);
   return (
     <div className="App">
       <RouterProvider router={router} />
--- src/features/product/components/ProductDetail.js
+++ src/features/product/components/ProductDetail.js
@@ -5,6 +5,8 @@ import { RadioGroup } from '@headlessui/react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { fetchAllProductByIdAsync, selectProductById } from '../productListSlice'
+import { selectLoggedInUser } from '../../auth/authSlice'
+import { addToCartAsync } from '../../Cart/cartSlice'
 //dummy data
 const colors = [
   { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
@@ -33,12 +35,20 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+
+
 export default function ProductDetail() {
   const [selectedColor, setSelectedColor] = useState(colors[0])
   const [selectedSize, setSelectedSize] = useState(sizes[2])
+  const user = useSelector(selectLoggedInUser)
   const product = useSelector(selectProductById)
   const dispatch = useDispatch()
   const params = useParams();
+
+  const handleCart = (e)=>{
+    e.preventDefault(); 
+    dispatch(addToCartAsync({...product,quantity:1,user: user.id}))
+  }
 
   useEffect(()=>{
     dispatch(fetchAllProductByIdAsync(params.id))
@@ -242,6 +252,7 @@ export default function ProductDetail() {
               </div>
 
               <button
+                onClick={handleCart}
                 type="submit"
                 className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
